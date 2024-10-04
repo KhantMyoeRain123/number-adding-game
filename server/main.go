b2d9ede8398d2728b2a256bf3b1f984a6117a514
KhantMyoeRain123 <rain8182014@gmail.com>
2024-10-04 22:45:41 +0900

    Implemented graceful shutdown for server

--- server/main.go
+++ server/main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gorilla/websocket"
 )
 
 func main() {
@@ -11,9 +16,21 @@ func main() {
 }
 
 func setupAPI(mux *http.ServeMux) {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	gameServer := NewGameServer()
 	fs := http.FileServer(http.Dir("dist"))
 	mux.Handle("/", fs)
+
+	defer func() {
+		for player := range gameServer.PlayerList {
+			log.Println("Closing connection for " + player.PlayerId)
+			player.Connection.WriteMessage(
+				websocket.CloseMessage,
+				websocket.FormatCloseMessage(websocket.CloseGoingAway, "Closing connection for "+player.PlayerId),
+			)
+		}
+	}()
 
 	//register the handlers to each route
 	mux.HandleFunc("/api", ApiRoot)
@@ -23,9 +40,13 @@ func setupAPI(mux *http.ServeMux) {
 	mux.HandleFunc("POST /api/room/make", gameServer.MakeRoom)
 	mux.HandleFunc("GET /api/room/get/{roomCode}", gameServer.GetRoom)
 
-	log.Println("Server is running on http://localhost:8080")
-	err := http.ListenAndServe(":8080", mux)
-	if err != nil {
-		log.Fatal(err)
-	}
+	go func() {
+		log.Println("Server is running on http://localhost:8080")
+		err := http.ListenAndServe(":8080", mux)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	<-sigChan
 }
